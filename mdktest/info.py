3f1ca54b9775681131eaa7f6b2173a4b8f52e1fb
Kelsie King <kelsieking23@vt.edu>
2024-06-24 16:02:36 -0700

    added info.py

---
+++ mdktest/info.py
@@ -0,0 +1,12 @@
+'''
+This is Information
+==================================
+this module contains functions that do something. 
+'''
+
+def summary(*universe):
+    pass
+
+def extract(universe):
+    pass
+
